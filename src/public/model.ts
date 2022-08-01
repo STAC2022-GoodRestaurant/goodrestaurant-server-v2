7f37a27c4aff530805ee0c16685598efcf84ba3d
Ko-GyeongTae <kokt0203@naver.com>
2022-08-01 21:23:38 +0900

    Basic Model 추가

---
+++ src/public/model.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Model {
+  @PrimaryGeneratedColumn("increment")
+  id!: number;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ nullable: true })
+  deletedAt?: Date;
+}
