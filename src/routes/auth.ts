a83de767ab962f454e8d892bd4d9ec6b4920b6d4
Ko-GyeongTae <kokt0203@naver.com>
2022-08-22 21:40:01 +0900

    auth api 수정

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,57 @@
 import { Request, Response, NextFunction, Router } from "express";
+import { body } from "express-validator";
+import { UserModel } from "../models/user.mo";
+import { validator } from "../public/middleware";
+import { AppDataSource } from "../utils/rds";
 
 export const authPath = "/auth";
 export const authRouter = Router();
 
 authRouter.post(
-  "/sign-up",
+  "/sign-in",
+  validator([body("email").exists(), body("password").exists()]),
   async (req: Request, res: Response, next: NextFunction) => {
     console.log(req.body);
-    res.status(200).json(req.body).end();
+    res.json(req.body).end();
+  }
+);
+
+authRouter.post(
+  "/sign-up",
+  validator([
+    body("email").exists(),
+    body("name").exists(),
+    body("password").exists(),
+  ]),
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { email, name, password } = req.body;
+
+    const userRepository = AppDataSource.getRepository(UserModel);
+
+    await userRepository.create({
+      email,
+      name,
+      password,
+    });
+
+    res.json(req.body).end();
+  }
+);
+
+authRouter.get(
+  "/check-email",
+  validator([body("email").exists()]),
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    res.json(req.body).end();
+  }
+);
+
+authRouter.delete(
+  "/cancel",
+  validator([body("email").exists()]),
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.body);
+    res.json(req.body).end();
   }
 );
