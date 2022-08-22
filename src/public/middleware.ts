21ee8189328491aa67c917531cc98b4abeecdae5
Ko-GyeongTae <kokt0203@naver.com>
2022-08-22 21:39:40 +0900

    미들웨어 추가

--- src/public/middleware.ts
+++ src/public/middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { validationResult } from "express-validator";
 
-export const validate = (validations: any[]) => {
+export const validator = (validations: any[]) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     await Promise.all(validations.map((validation) => validation.run(req)));
 
