739641298c0c2eef2e968868450aa2890638e03f
Ko-GyeongTae <kokt0203@naver.com>
2022-09-10 17:15:08 +0900

    임시

--- src/init.ts
+++ src/init.ts
@@ -1,10 +1,15 @@
+import axios from "axios";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
-import pino from "express-pino-logger";
+// import pino from "express-pino-logger";
 import useragent from "express-useragent";
 import helmet from "helmet";
 import createError, { HttpError } from "http-errors";
+import path from "path";
 import { authPath, authRouter } from "./routes/auth";
+import { couponPath, couponRouter } from "./routes/coupon";
+import { filePath, fileRouter } from "./routes/file";
+import { restaurantPath, restaurantRouter } from "./routes/restaurant";
 import { logger } from "./utils/logger";
 
 export const initApp = () => {
@@ -16,7 +21,7 @@ export const initApp = () => {
   app.use(useragent.express());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use(pino());
+  // app.use(pino());
   app.use(cors());
 
   // route
@@ -25,14 +30,32 @@ export const initApp = () => {
   });
 
   app.use(authPath, authRouter);
+  app.use(couponPath, couponRouter);
+  app.use(restaurantPath, restaurantRouter);
+  app.use(filePath, fileRouter);
 
   app.use(
-    (
+    async (
       err: Error | HttpError,
       _req: Request,
       res: Response,
       _next: NextFunction
     ) => {
+      await axios
+        .post(
+          process.env.DISCORD_WEBHOOK_URL || "",
+          {
+            content:
+              "착한식당 good-restaurant-v2 에서 **issue**가 발생하였습니다! \n" +
+              err.name +
+              "\n" +
+              err.message,
+          },
+          {}
+        )
+        .catch((err) => {
+          console.log(err);
+        });
       if (createError.isHttpError(err)) {
         if (err.statusCode >= 500) {
           logger.error(err.message);
